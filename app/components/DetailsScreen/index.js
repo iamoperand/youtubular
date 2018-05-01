2b0704dbb28615aacb7a7522a2a141268e408415
iamoperand <narora200@gmail.com>
2018-05-01 20:49:49 +0530

    screen title added

--- app/components/DetailsScreen/index.js
+++ app/components/DetailsScreen/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 
 export default class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Details'
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
